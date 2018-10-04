607bfd491cd9c23fb2da5c9afdf19695d0551fc7
zedlad <32540498+zedlabs@users.noreply.github.com>
2018-10-04 13:32:43 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/android/intouch_android/MainActivity.java
+++ app/src/main/java/com/example/android/intouch_android/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            //implement the fab to open the desired activity
 
         BottomNavigationView bottomNavigationView= findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
